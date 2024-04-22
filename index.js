9bd2763b9b3473659be8ff8db15698844d6bf354
TK1987265 <qcc2016@yahoo.com>
2024-04-22 16:07:31 -0400

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+// Function that creates thank-you cards for a given array of names and an event
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function that counts down from a given positive integer to zero
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--; 
+    }
+  }
+  
+  // Example use of writeCards()
+  const thankYouMessages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(thankYouMessages);
+  
+  // Example use of countDown()
+  countDown(10);
+  
